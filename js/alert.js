3003e2716a15ff0ae93e68d3e06b32ed61add7b2
Olena Roganova <132047130+olenaroganova@users.noreply.github.com>
2023-07-08 14:24:55 +0300

    2

---
+++ js/alert.js
@@ -0,0 +1,29 @@
+alert("Привіт");
+alert("World");
+
+alert(1 + 8 + 5);
+
+//here comentar
+
+/*comentear
+here
+and here */
+
+let message;
+message = "BTS";
+alert(message);
+
+// let message = "Jimin";
+// alert(message);
+
+// let user = "Ivan";
+// let age = 25;
+// let message = "BTS";
+
+let admin, name;
+name = "Іван";
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentVisitor = "Anton";
